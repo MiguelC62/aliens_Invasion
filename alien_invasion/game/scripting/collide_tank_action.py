c95b77f1184831f06f6437af9557886c83c1953c
pr0gramama <paleofemme@gmail.com>
2022-03-31 11:44:18 -0600

    Miguel Secades lives added

--- alien_invasion/game/scripting/collide_tank_action.py
+++ alien_invasion/game/scripting/collide_tank_action.py
@@ -15,22 +15,22 @@ class CollideTankAction(Action):
     def execute(self, cast, script, callback):
         bomb = cast.get_first_actor(BOMBS_GROUP)
         tank = cast.get_first_actor(TANK_GROUP)
-        projectil = cast.get_first_actor(PROJECTILS_GROUP)
+        # projectil = cast.get_first_actor(PROJECTILS_GROUP)
         
         bomb_body = bomb.get_body()
         tank_body = tank.get_body()
 
         if self._physics_service.has_collided(bomb_body, tank_body):
-            #ball.bounce_y()
-            sound = Sound(BOUNCE_SOUND)
-            self._audio_service.play_sound(sound)
-            #cast.clear_actors(TANK_GROUP)
-            projectil.stop_moving()
-            bomb.stop_moving()
-            cast.clear_actors(DIALOG_GROUP)
-            text = Text(WAS_GOOD_GAME, FONT_FILE, FONT_SMALL, ALIGN_CENTER)
-            position = Point(CENTER_X, CENTER_Y)
-            label = Label(text, position)
-            cast.add_actor(DIALOG_GROUP, label)
-            self._audio_service.release()
+            stats = cast.get_first_actor(STATS_GROUP)
+            stats.lose_life()
+
+
+            if stats.get_lives() > 0:
+                sound = Sound(BOUNCE_SOUND)
+                self._audio_service.play_sound(sound)
+                callback.on_next(TRY_AGAIN)
+            else:
+                callback.on_next(GAME_OVER)
+                sound = Sound(OVER_SOUND)
+                self._audio_service.play_sound(sound)
     
